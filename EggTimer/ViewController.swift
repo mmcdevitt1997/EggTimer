c673d8ab79b4a36d20db5f53fe29a3e804ac8cf4
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-02-25 11:22:01 -0600

    about to start on timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = ["Soft": 5, "Medium":7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        print(eggTimes[hardness]!)
+    
+    }
+    
 
 }
